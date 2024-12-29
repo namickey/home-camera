917e547588276d4ceb48fa2060792dc954fd98cc
namickey <teethdai@gmail.com>
2024-12-29 21:46:03 +0900

    chmod

--- 6.tracker/3.check_log.sh
+++ 6.tracker/3.check_log.sh
@@ -6,6 +6,6 @@ else
   echo 'tracker is out of home.' | tee -a app.log
   ./6.create-stopfile.sh
   touch ./done.txt
-  ./4.line-push-papa.sh
-  ./5.line-push-mama.sh
+  #./4.line-push-papa.sh
+  #./5.line-push-mama.sh
 fi
